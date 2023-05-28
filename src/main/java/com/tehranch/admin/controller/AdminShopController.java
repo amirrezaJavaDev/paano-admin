33464e0ece66936da1bd200f6b161d72131065bf
reza <amirrezazargar.1313@yahoo.com>
2023-05-28 21:55:09 +0330

    c/back2: Exception fixed - validation checked

--- src/main/java/com/tehranch/admin/controller/AdminShopController.java
+++ src/main/java/com/tehranch/admin/controller/AdminShopController.java
@@ -2,11 +2,13 @@ package com.tehranch.admin.controller;
 
 import com.tehranch.admin.dto.ownershop.ShopOwnerDto;
 import com.tehranch.admin.service.AdminShopService;
+import com.tehranch.admin.service.MapValidationErrorService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -16,13 +18,16 @@ import javax.validation.Valid;
 @AllArgsConstructor
 public class AdminShopController {
     private final AdminShopService adminShopService;
+    private final MapValidationErrorService mapValidationErrorService;
 
     @PostMapping("/add")
     public ResponseEntity<?> addShop(
             @RequestHeader(HttpHeaders.AUTHORIZATION)String encoding,
-            @Valid @RequestBody ShopOwnerDto shopOwnerDto){
-        Long result = adminShopService.save(encoding, shopOwnerDto);
-        return new ResponseEntity<>("created shop with id:"+result, HttpStatus.ACCEPTED);
+            @Valid @RequestBody ShopOwnerDto shopOwnerDto, BindingResult result){
+        ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
+        if(errorMap!=null) return errorMap;
+        Long resultId = adminShopService.save(encoding, shopOwnerDto);
+        return new ResponseEntity<>("created shop with id:"+resultId, HttpStatus.ACCEPTED);
     }
 
 }
